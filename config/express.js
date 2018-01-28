f9a11f8916659b300d7fa075b7b5874f7f9ea049
collinsnji <collins.geek@gmail.com>
2018-01-27 20:15:26 -0500

    Change view engine to jade

--- config/express.js
+++ config/express.js
@@ -25,8 +25,9 @@ module.exports = function(app) {
   app.enable('trust proxy');
 
   // Configure Express
-  app.set('view engine', 'ejs');
-  require('ejs').delimiter = '$';
+  app.engine('pug', require('pug').__express);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug');
   app.use(bodyParser.urlencoded({
     extended: true
   }));
